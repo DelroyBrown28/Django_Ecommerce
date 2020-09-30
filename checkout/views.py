0125ff04f01347cd8d3109b79f052f8d56a00867
Delroy Brown <Delroy_jb@hotmail.co.uk>
2020-09-30 15:31:29 +0100

    added stripe payments

--- checkout/views.py
+++ checkout/views.py
@@ -13,6 +13,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51HX5u6A1fnSIB3Km43MupzPhDdzE3QyyAOUnwsbTnovphUUDBuyIEDZDXo3rNw5SQHBkDuqo3keliCWShvZ3PGdN00gKgCOG4V',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
