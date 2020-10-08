d9f9b634189befb2a965c0b568b503ea91c67518
Delroy Brown <Delroy_jb@hotmail.co.uk>
2020-10-08 23:44:59 +0100

    fixed email issue

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -24,11 +24,11 @@ class StripeWH_Handler:
             {'order': order})
         body = render_to_string(
             'checkout/confirmation_emails/confirmation_email_body.txt',
-            {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL})
+            {'order': order, 'contact_email': config('DEFAULT_FROM_EMAIL')})
         send_mail(
             subject,
             body,
-            settings.DEFAULT_FROM_EMAIL,
+            config('DEFAULT_FROM_EMAIL'),
             [cust_email]
         )
 
