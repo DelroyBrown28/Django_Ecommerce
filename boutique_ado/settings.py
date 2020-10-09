906d413dd6742db7e58eb55ca33a8f9beee32206
Delroy Brown <Delroy_jb@hotmail.co.uk>
2020-10-09 10:05:42 +0100

    Deploying to Heroku

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -10,10 +10,10 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 from decouple import config
 from django import forms
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 
 # Quick-start development settings - unsuitable for production
