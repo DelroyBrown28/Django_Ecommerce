ae937eb23baa7c2658e96357b0643bf9f0e8d9fd
Delroy Brown <Delroy_jb@hotmail.co.uk>
2020-10-10 15:35:52 +0100

    NO MAJOR CHANGES...SAVING

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -179,6 +179,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+
 if 'USE_AWS' in os.environ:
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'dbrown-boutique-ado'
