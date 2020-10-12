27998661b597b3cf15083d1a8cc9203336e9a95c
Delroy Brown <Delroy_jb@hotmail.co.uk>
2020-10-12 10:23:18 +0100

    aws

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = 'q7llpVpFKaVgwO9LabSgPViTiBQGDbd8YqcqB1XC'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'development' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '29bc124dd1a5.ngrok.io',
