eb1cdfa3688b581f3e71d9533d816be0a3511045
Delroy Brown <Delroy_jb@hotmail.co.uk>
2020-10-16 15:49:07 +0100

    NO MAJOR CHANGES...SAVING

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-SECRET_KEY = "q7llpVpFKaVgwO9LabSgPViTiBQGDbd8YqcqB1XC"
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
