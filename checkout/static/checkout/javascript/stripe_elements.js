96a16f2ee303ecd6a68cd4f814ce1a380ab13dea
Delroy Brown <Delroy_jb@hotmail.co.uk>
2020-10-16 15:21:15 +0100

    updated js file

--- checkout/static/checkout/javascript/stripe_elements.js
+++ checkout/static/checkout/javascript/stripe_elements.js
@@ -59,6 +59,7 @@ form.addEventListener('submit', function (ev) {
         'save_info': saveInfo,
     };
     var url = '/checkout/cache_checkout_data/';
+    console.log(url);
 
     $.post(url, postData).done(function () {
         stripe.confirmCardPayment(clientSecret, {
