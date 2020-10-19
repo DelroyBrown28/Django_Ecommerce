c4f8398533d6f7386582829fc907d43e9920dfc1
Delroy Brown <Delroy_jb@hotmail.co.uk>
2020-10-19 15:16:10 +0100

    updated bugs

--- checkout/views.py
+++ checkout/views.py
@@ -171,7 +171,7 @@ def checkout_success(request, order_number):
         Your order number is {order_number}. A confirmation \
         email will be sent to {order.email}.')
 
-    template = 'checkout/checkout.html'
+    template = 'checkout/checkout_success.html'
     context = {
         'order': order,
     }
