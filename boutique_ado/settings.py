d53b4a51202ece44f464a37708d2977ded089b87
Delroy Brown <delroybrown.db@gmail.com>
2021-05-07 13:38:06 +0100

    Added and updated templates for frontend backend

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'wagtail.documents',
     'wagtail.images',
     'wagtail.search',
+    'frontend_admin',
     'wagtail.admin',
     'wagtail.core',
     'wagtailfontawesome',
--- home/views.py
+++ home/views.py
@@ -4,21 +4,17 @@ from .models import HomePageCustomisation
 
 def index(request):
     home_page_customisation = HomePageCustomisation.objects.all()
-
     context = {
         'home_page_customisation' : home_page_customisation,
 
     }
-
     return render(request, 'home/index.html', context)
 
 
 def NavbarCustomisation(request):
     navbar_customisation = NavbarCustomisation.objects.all()
-
     context = {
         'navbar_customisation' : navbar_customisation,
 
     }
-
     return render(request, 'includes/main_nav.html', context)
